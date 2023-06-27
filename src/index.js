46f9e75afe886f8c4bc45b48b17e97920a97176a
Rubesh K R <rupesh.maxpain@gmail.com>
2023-06-27 08:53:56 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>React App</h1>
+    <ul>
+      <li>list one</li>
+      <li>list two</li>
+      <li>list three</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
