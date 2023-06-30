74880aa482de24d8fc4696fb9ebc8151540ec297
Rubesh K R <rupesh.maxpain@gmail.com>
2023-06-30 23:12:01 +0530

    inline styling

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const img = "https://picsum.photos/200";
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
 
 ReactDOM.render(
   <div>
-    <h1 className="heading" contentEditable="true" spellCheck="false">
+    <h1
+      style={customStyle}
+      className="heading"
+      contentEditable="true"
+      spellCheck="false"
+    >
       React App
     </h1>
-    <img atr="random" src={img} />
+    <img alt="random" src={img + "?grayscale"} />
   </div>,
   document.getElementById("root")
 );
-
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
